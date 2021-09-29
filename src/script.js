9d8c7e82c60e7e30b6d4a55d55146c8ec02028c6
Victor Shalamov <77752535+friven311@users.noreply.github.com>
2021-09-30 01:33:21 +0300

    added es6 script

--- src/script.js
+++ src/script.js
@@ -1,26 +1,20 @@
-function Parrent(type, title, text) {
-     "use strict";
-     this.type = type;
-     this.title = title;
-     this.text = text;
+ class Parrent {
+   constructor(type, title, text) {
+   this.type = type;
+   this.title = title;
+   this.text = text;
    }
-
-   Parrent.prototype = {
-     constructor: Parrent,
-   };
-   
-   Parrent.prototype.getTemplate = function () {
-     return ``;
-   };
-   
-   Parrent.prototype.show = function () {
-     var cont = document.querySelector(`.contMod`);
-     if (cont) {
-          cont.innerHTML += this.getTemplate();
-     }
-   };
-   
-   Parrent.prototype.hide = function () {
-     var cont = document.querySelector(`.${this.type}`); 
-     cont.remove()
-     }
+   getTemplate () {
+      return ``;
+   }
+   show () {
+      const cont = document.querySelector(`.contMod`);
+      if (cont) {
+         cont.innerHTML += this.getTemplate();
+      }
+   }
+   hide () {
+      const cont = document.querySelector(`.${this.type}`); 
+      cont.remove()
+      }
+ }
